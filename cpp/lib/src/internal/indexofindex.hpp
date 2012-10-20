6fb7fbafb866a8415a5cbe12da0ea242363b526a
Eugene V. Lyubimkin <jackyf.devel@gmail.com>
2012-10-20 15:22:58 +0300

    WIP

---
+++ cpp/lib/src/internal/indexofindex.hpp
@@ -0,0 +1,39 @@
+/**************************************************************************
+*   Copyright (C) 2012 by Eugene V. Lyubimkin                             *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License                  *
+*   (version 3 or above) as published by the Free Software Foundation.    *
+*                                                                         *
+*   This program is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU General Public License for more details.                          *
+*                                                                         *
+*   You should have received a copy of the GNU GPL                        *
+*   along with this program; if not, write to the                         *
+*   Free Software Foundation, Inc.,                                       *
+*   51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA               *
+**************************************************************************/
+
+namespace cupt {
+namespace internal {
+namespace ioi {
+
+struct Record
+{
+	uint32_t* offsetPtr;
+	string* packageNamePtr;
+	const char* providesBegin;
+	const char* providesEnd;
+};
+// index suffix number must be incremented every time Record changes
+
+typedef std::function< void () > Callback;
+
+void processIndex(const string& path, const Callback&, Record*);
+
+}
+}
+}
+
