dda4c1155ba47b10a61d51202dfc205e35b85318
Eugene V. Lyubimkin <jackyf.devel@gmail.com>
2012-10-20 14:43:49 +0300

    WIP

--- cpp/lib/src/internal/indexofindex.cpp
+++ cpp/lib/src/internal/indexofindex.cpp
@@ -30,8 +30,8 @@ struct Record
 {
 	uint32_t* offsetPtr;
 	const string* packageNamePtr;
-	const char* providesValueBegin;
-	uint32_t providesValueLength;
+	const char* providesBegin;
+	const char* providesEnd;
 };
 // index suffix number must be incremented every time Record changes
 
@@ -56,59 +56,92 @@ void parseFullIndex(const string& path, const Callback& callback, Record* record
 		fatal2(__("unable to open the file '%s': %s"), path, openError);
 	}
 
-	try
-	{
-		decltype(*(record->offsetPtr)) offset = 0;
+	decltype(*(record->offsetPtr)) offset = 0;
 
-		while (true)
+	while (true)
+	{
+		const char* buf;
+		size_t size;
+		auto getNextLine = [&file, &buf, &size, &prePackageRecord]
 		{
-			const char* buf;
-			size_t size;
-			auto getNextLine = [&file, &buf, &size, &prePackageRecord]
-			{
-				file.rawGetLine(buf, size);
-				offset += size;
-			};
+			file.rawGetLine(buf, size);
+			offset += size;
+		};
 
-			getNextLine();
-			if (size == 0)
-			{
-				break; // eof
-			}
-
-			static const size_t packageAnchorLength = sizeof("Package: ") - 1;
-			if (size > packageAnchorLength && !memcmp("Package: ", buf, packageAnchorLength))
-			{
-				record.packageNamePtr->assign(buf + packageAnchorLength, size - packageAnchorLength - 1);
-			}
-			else
-			{
-				fatal2(__("unable to find a Package line"));
-			}
-
-			record.providesValueBegin = nullptr;
-			while (getNextLine(), size > 1)
-			{
-				static const size_t providesAnchorLength = sizeof("Provides: ") - 1;
-				if (*buf == 'P' && size > providesAnchorLength && !memcmp("rovides: ", buf+1, providesAnchorLength-1))
-				{
-					record.providesValueBegin = buf + providesAnchorLength;
-					record.providesValueLength = buf + size - 1;
-				}
-			}
-			*(record->offsetPtr) = offset;
-			callback();
+		getNextLine();
+		if (size == 0)
+		{
+			break; // eof
 		}
-	}
-	catch (Exception&)
-	{
-		fatal2(__("unable to parse the index '%s'"), alias);
+		*(record->offsetPtr) = offset;
+
+		static const size_t packageAnchorLength = sizeof("Package: ") - 1;
+		if (size > packageAnchorLength && !memcmp("Package: ", buf, packageAnchorLength))
+		{
+			record.packageNamePtr->assign(buf + packageAnchorLength, size - packageAnchorLength - 1);
+		}
+		else
+		{
+			fatal2(__("unable to find a Package line"));
+		}
+
+		record.providesValueBegin = nullptr;
+		while (getNextLine(), size > 1)
+		{
+			static const size_t providesAnchorLength = sizeof("Provides: ") - 1;
+			if (*buf == 'P' && size > providesAnchorLength && !memcmp("rovides: ", buf+1, providesAnchorLength-1))
+			{
+				record.providesValueBegin = buf + providesAnchorLength;
+				record.providesValueEnd = buf + size - 1;
+			}
+		}
+		callback();
 	}
 }
 
-void parseIndexOfIndex(const string& path, const Callback& callback)
-{
+namespace field {
 
+const char provides = 'p';
+
+}
+
+void parseIndexOfIndex(const string& path, const Callback& callback, Record* record)
+{
+	string openError;
+	File file(path, "r", openError);
+	if (!openError.empty())
+	{
+		fatal2(__("unable to open the file '%s': %s"), path, openError);
+	}
+
+	const char* buf;
+	size_t bufSize;
+	while (file.rawGetLine(buf, bufSize), bufSize > 0)
+	{
+		{ // offset and package name:
+			if (bufSize < 6)
+			{
+				fatal2i("ioi: offset and package name: too small line");
+			}
+			(*record->offsetPtr) = ntohl(*reinterpret_cast< const uint32_t* >(buf));
+			record->packageNamePtr->assign(buf+sizeof(uint32_t), buf+bufSize-1);
+			break;
+		}
+		while (file.rawGetLine(buf, bufSize), bufSize > 1)
+		{
+			auto fieldType = buf[0];
+			switch (fieldType)
+			{
+				case field::provides:
+					record->providesBegin = buf+1;
+					record->providesEnd = but+bufSize-1;
+					break;
+				default:
+					fatal2i("ioi: invalid field type %zu", size_t(fieldType));
+			}
+		}
+		callback();
+	}
 }
 
 }
